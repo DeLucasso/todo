f562a8470ecbfe4cc6efcd5d621508c9fc90689f
DeLucasso <dumdumcz@gmail.com>
2022-05-08 22:38:39 -0700

    Finished switch comparsion

--- app.js
+++ app.js
@@ -5,20 +5,51 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+  app.set('view engine', 'ejs');
+
   app.get("/", function(req, res) {
 
+  var day = "";
   var today = new Date();
-  var currentDay = today.getDate();
+  var currentDay = today.getDay();
 
-  if (currentDay === 6 || currentDay === 0){
-      res.write("<h1>Yay it's the weekend!</h1>");
-    } else {
-      res.sendFile(__dirname + "/index.html");
-  }
+switch (currentDay){
 
-  });
+  case 0:
+  day = "Sunday";
+  break;
 
+  case 1:
+  day = "Monday";
+  break;
 
+  case 2:
+  day = "Tuesday";
+  break;
+
+  case 3:
+  day = "Wednesday";
+  break;
+
+  case 4:
+  day = "Thursday";
+  break;
+
+  case 5:
+  day = "Friday";
+  break;
+
+  case 6:
+  day = "Saturday";
+  break;
+
+  default:
+  console.log("Err! No match in switch!");
+}
+
+  res.render("list", {kindOfDay: day});
+
+});
 
   app.listen(3000, function() {
     console.log("Server started on port 3000");
