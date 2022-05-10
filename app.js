1778dd4c1aeb1c586052090b2906dd8ba4b9ebbf
DeLucasso <dumdumcz@gmail.com>
2022-05-09 23:47:19 -0700

    Added functionality to add task in list.ejs using loop

--- app.js
+++ app.js
@@ -5,51 +5,38 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
   app.set('view engine', 'ejs');
+
+  app.use(bodyParser.urlencoded({extended: true}));
 
   app.get("/", function(req, res) {
 
-  var day = "";
   var today = new Date();
-  var currentDay = today.getDay();
 
-switch (currentDay){
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  };
 
-  case 0:
-  day = "Sunday";
-  break;
+  var day = today.toLocaleDateString("en-US", options);
 
-  case 1:
-  day = "Monday";
-  break;
-
-  case 2:
-  day = "Tuesday";
-  break;
-
-  case 3:
-  day = "Wednesday";
-  break;
-
-  case 4:
-  day = "Thursday";
-  break;
-
-  case 5:
-  day = "Friday";
-  break;
-
-  case 6:
-  day = "Saturday";
-  break;
-
-  default:
-  console.log("Err! No match in switch, your current day is" + currentDay);
-}
-
-  res.render("list", {kindOfDay: day});
+  res.render("list", {kindOfDay: day, newListItems: items});
 
 });
+
+app.post("/", function (req, res) {
+  item = req.body.newListItems;
+  items.push(item);
+
+  res.redirect("/");
+console.log("User input was " + item);
+console.log("Now the array items contains: " + items);
+});
+
 
   app.listen(3000, function() {
     console.log("Server started on port 3000");
