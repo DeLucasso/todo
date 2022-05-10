dc1d46f80bdda0e343b455ab96368e0567e61c2a
DeLucasso <dumdumcz@gmail.com>
2022-05-10 00:51:44 -0700

    Added CSS, Public folder and path to use public in express

--- app.js
+++ app.js
@@ -5,24 +5,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+let items = [];
 
   app.set('view engine', 'ejs');
 
   app.use(bodyParser.urlencoded({extended: true}));
+  app.use(express.static("public"));
 
   app.get("/", function(req, res) {
 
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: 'long',
     day: 'numeric',
     month: 'long',
     year: 'numeric'
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {kindOfDay: day, newListItems: items});
 
